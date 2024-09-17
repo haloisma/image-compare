d2b9064c3c0390c28847c041ac0f66d64cb40c31
haloisma <ismael.aparicio@halopowered.com>
2024-09-17 10:13:21 -0300

    folder structure change and adding the generateFileName

--- compareImages.js
+++ compareImages.js
@@ -2,9 +2,18 @@ import fs from "fs";
 import { PNG } from "pngjs";
 import pixelmatch from "pixelmatch";
 
-const img1Path = "../image-compare-1/image1.png"; // Path to your first image
-const img2Path = "../image-compare-1/image2.png"; // Path to your second image
-const diffPath = "../image-compare-1/diff.png"; // Path to save the diff image
+// Function to generate a unique file name with a timestamp
+function generateFileName(basePath, extension) {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-"); // Replace : and . to avoid issues in filenames
+  return `${basePath}_${timestamp}${extension}`;
+}
+
+const img1Path = "../image-compare-1/images_input_pixelmatch/image1.png"; // Path to your first image
+const img2Path = "../image-compare-1/images_input_pixelmatch/image2.png"; // Path to your second image
+const diffPath = generateFileName(
+  "../image-compare-1/pixelmatch_results/diff",
+  ".png"
+); // Path to save the diff image
 
 // Read the two images
 const img1 = PNG.sync.read(fs.readFileSync(img1Path));
@@ -16,10 +25,11 @@ const diff = new PNG({ width, height });
 
 // Compare the images
 const pixelDiff = pixelmatch(img1.data, img2.data, diff.data, width, height, {
-  threshold: 0.7, // Adjust sensitivity
+  threshold: 0.1, // Adjust sensitivity
 });
 
 // Write the diff image to disk
 fs.writeFileSync(diffPath, PNG.sync.write(diff));
 
 console.log(`Number of differing pixels: ${pixelDiff}`);
+console.log(`Diff file saved as: ${diffPath}`);
