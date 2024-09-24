9591eda24c10c50931a633943a2624b90bfb1049
Mateo Alberti <mateo.alberti@gmail.com>
2024-09-24 17:37:27 -0300

    Update screenshot to create a folder for every screen and to be executable in the terminal

--- screenshot.js
+++ screenshot.js
@@ -1,12 +1,26 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
+import path from 'path';
+import fs from 'fs';
+
+// Get the pageName from the command-line arguments
+const pageName = process.argv[2];
+
+if (!pageName) {
+  console.error('Please provide a page name as an argument.');
+  process.exit(1);
+}
 
 (async () => {
   // Launch a new browser instance
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  // Construct the URL and file path based on the page name
+  const url = 'https://www.' + pageName;
+  const screenshotPath = `screens/${pageName}/screenshot.png`;
+
   // Navigate to the web page
-  await page.goto("https://www.google.com.uy/");
+  await page.goto(url);
 
   // Set the viewport to a specific resolution
   await page.setViewport({
@@ -15,11 +29,22 @@ const puppeteer = require("puppeteer");
     deviceScaleFactor: 1, // Ensures the image is captured at the same resolution
   });
 
-  // Capture the screenshot
-  await page.screenshot({ path: "webpage.png", fullPage: true });
+  // Ensure the directory exists
+  await ensureDirectoryExists(screenshotPath);
 
-  console.log("Screenshot saved as webpage.png");
+  // Capture the screenshot
+  await page.screenshot({ path: screenshotPath, fullPage: true });
+
+  console.log("Screenshot saved as screenshot.png in the folder "+ screenshotPath );
 
   // Close the browser
   await browser.close();
 })();
+
+// Helper function to ensure directory exists
+async function ensureDirectoryExists(filePath) {
+  const dir = path.dirname(filePath);
+  if (!fs.existsSync(dir)) {
+    await fs.promises.mkdir(dir, { recursive: true });
+  }
+}
